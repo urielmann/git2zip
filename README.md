dbc19d9be9b402bb8303e9993fc4673851ee067d
Uri Mann <abba.mann@gmail.com>
2022-03-03 19:00:09 -0800

    Add README and LICENSE

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # git2zip
 Script to create a backup zip of latest Git commits
+
+## Arguments
+>**--repository** - Path to repository to archive (default: current directory)  
+>**--branch** - Branch to archive (default: *master*)  
+>**--num-commits** - Number of commits to archive (default: 1). The archive will contain all files from the *N* last commits.  
+>**--archive** - Path to .zip file to create (default: *backup.zip*)  
--- git2zip.py
+++ git2zip.py
@@ -1,13 +1,30 @@
+#########################################################################
+# Copyright 2022 (c), Uri Mann. All rights reserved.                    #
+# mailto:abba.mann@gmail.com                                            #
+#                                                                       #
+# This program is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU Affero Public License version 3 as      #
+# published by the Free Software Foundation.                            #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+# See the GNU Affero Public License for more details.                   #
+#                                                                       #              
+# You should have received a copy of the GNU Affero Public License      #
+# along with this program.  If not, see http://www.gnu.org/licenses.    #
+#########################################################################
+
 import os
 import git
 import click
 import zipfile
 
 @click.command()
-@click.option('--repository', default='D:/CrowdStrike/private/main', help='Repository path')
+@click.option('--repository', default='.', help='Repository path')
 @click.option('--branch', default='main', help='Branch name')
-@click.option('--num-commits', default=1, help='Number of commits to zip')
-@click.option('--archive', default='backup.zip', help='Path to zip')
+@click.option('--num-commits', default=1, help='Number of commits to archiive')
+@click.option('--archive', default='backup.zip', help='Path to .zip (archive) file')
 def main(repository, branch, num_commits, archive):
     repo = git.Repo(path=repository, search_parent_directories=True)
     # get all of the commits on the branch
@@ -20,10 +37,8 @@ def main(repository, branch, num_commits, archive):
     with zipfile.ZipFile(archive, 'w', zipfile.ZIP_DEFLATED) as zip:
         # for each commit get the paths to the commited files
         for commit in commits:
-            # print(i, len(commit.stats.files),
-            #          commit.message,
-            #          commit.stats.files.keys())
             for file in commit.stats.files.keys():
+                # skip duplicates
                 if file in archived: continue
                 archived.add(file)
                 print(file)
